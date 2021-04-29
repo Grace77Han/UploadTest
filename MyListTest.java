74edd03ee9810293db00a1e31456d0c56ccd2d8d
chilkwon <chilhoank@yahoo.ca>
2021-04-28 20:46:56 -0400

    Added MyListTest.java to be completed

---
+++ MyListTest.java
@@ -0,0 +1,14 @@
+
+public class MyListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		//1. write some code to use MyList
+		//2 .use ArrayList as data storage
+		
+		
+	}
+
+}
