cda3c85019498200e382fab54c4bd942955b9b8f
chilkwon <chilhoan.kwon@candu.org>
2021-06-29 09:21:39 -0400

    Added solution to the ArrayListExample

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -1,40 +1,38 @@
 import java.util.ArrayList;
-import java.util.Iterator; 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 public class ArrayListExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<String> chilDrivingData = new ArrayList<>();
-		
-	    String[][] chilDrivingSampleData = {
-	    		{"Monday","8:10","8:30"},
-	    		{"Tuesday","8:20","8:40"},
-	    		{"Wednesday","8:30","8:50"},
-	    		{"Thursday","8:34","8:54"},
-	    		{"Firday","8:21","8:41"},
-	    		{"Saturday","8:19","8:39"},
-	    };
 
-	    //Store the sampledata into the ArrayList
-		for (int i = 0 ; i < chilDrivingSampleData.length ; i++ ){
-			 chilDrivingData.add(chilDrivingSampleData[i]);  //<-- Research more about using add method, 
-		 }
-	    
-		//First of all, you can not add the sample data to ArrayList with the above way. 
-		// iterating means that retrieve all stored data
-		
-	    // iterating all elements from the ArrayList // I don't understand the exact meaning of iterating in this context. Do you want me to print all elements?
-	    // use Iterator
-		// I put Enumeration and Collection docs within the readme.MD file
-		Iterator<String> chilDrivingDataIterator = chilDrivingData.iterator();
+		String[][] chilDrivingSampleData = {
+				{"Monday","8:10","8:30"},
+				{"Tuesday","8:20","8:40"},
+				{"Wednesday","8:30","8:50"},
+				{"Thursday","8:34","8:54"},
+				{"Firday","8:21","8:41"},
+				{"Saturday","8:19","8:39"},
+		};
 
-	    // use Enumeration and Collections // I don't know what it Enumeration. Could you explain about it?
-	    
-	    
-	    
-	    
-	    
+		System.out.println(chilDrivingSampleData.length);
+
+		int capacity = chilDrivingSampleData.length;
+
+		String[] strArray1 =new String[capacity];
+
+		for(int i=0; i<chilDrivingSampleData.length;i++) {
+
+			strArray1[i]= chilDrivingSampleData[i][0]+","+chilDrivingSampleData[i][1]+","+chilDrivingSampleData[i][2];
+
+			chilDrivingData.add(strArray1[i]);
+		}
+
+		Enumeration<String> e = Collections.enumeration(chilDrivingData);
+		while(e.hasMoreElements())
+			System.out.println(e.nextElement());
 	}
 
 }
